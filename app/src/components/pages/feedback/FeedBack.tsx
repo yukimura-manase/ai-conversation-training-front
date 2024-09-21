f84d102512bc7f455541c61617623f10d06086f6
Masahiro Suda <masahirosuda@EH-PC086.local>
2024-09-21 14:20:01 +0900

    add: components-dir

---
+++ app/src/components/pages/feedback/FeedBack.tsx
@@ -0,0 +1,7 @@
+export const FeedBackPage = () => {
+  return (
+    <div>
+      <h2>FeedBackPage</h2>
+    </div>
+  );
+};
---
+++ app/src/constants/ColorPalette.ts
@@ -0,0 +1,7 @@
+export const ColorPalette = {
+  red: "#EF4434",
+  gray01: "#f4f5f6",
+  gray02: "#edf0f4",
+  gray03: "#d3dbe2",
+  gray04: "#8497a8",
+};
